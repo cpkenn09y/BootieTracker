45e50ff1237eb575adb9d1b07316704852e79c4e
cpkenn09y <kenyu@ucdavis.edu>
2013-12-14 12:16:44 -0800

    updated with master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131214092723) do
+ActiveRecord::Schema.define(:version => 20131214185624) do
+
+  create_table "authentications", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "cohorts", :id => false, :force => true do |t|
     t.integer  "c_id"
