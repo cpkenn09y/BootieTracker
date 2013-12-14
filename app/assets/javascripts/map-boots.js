233ea5ce3bcf4db182d16e7730b7e5684b42799a
cpkenn09y <kenyu@ucdavis.edu>
2013-12-14 11:59:11 -0800

    ken/elaine: info box is created upon hover
    * use font awesome icons
    * change root route to maps#index
    * info box is created containing a boot's information

--- app/assets/javascripts/map-boots.js
+++ app/assets/javascripts/map-boots.js
@@ -11,7 +11,7 @@ function centersMapOnYourLocation() {
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
+       position.coords.longitude);
 
       var infowindow = new google.maps.InfoWindow({
         map: map,
@@ -37,9 +37,9 @@ function grabBootsDataForMap() {
 
 function setPointsOntoMap(bootsDataForMap) {
   for(var i = 0, num = bootsDataForMap.length; i < num; i++) {
-    var lat = bootsDataForMap[i].user_name.latitude
-    var lon = bootsDataForMap[i].user_name.longitude
-    var name = bootsDataForMap[i].user_name.name
+    var lat = bootsDataForMap[i].user.latitude
+    var lon = bootsDataForMap[i].user.longitude
+    var name = bootsDataForMap[i].user.name
     var user = new google.maps.LatLng(lat, lon);
     var marker = new google.maps.Marker({
       position: user,
@@ -52,11 +52,31 @@ function setPointsOntoMap(bootsDataForMap) {
 
 function attachListenerOntoMarker(bootData, marker) {
   google.maps.event.addListener(marker, "mouseover", function(event) {
-    debugger
     console.log(bootData)
     console.log(marker)
     // debugger
+
+    createInfoWindowUponHover(bootData, marker)
   })
+}
+
+function createInfoWindowUponHover(bootData, marker) {
+  var contentString = '<div class="content-box">'+
+    '<p>'+bootData.user.name+'</p>'+
+    '<a href=mailto:'+bootData.user.email+'><i class="fa fa-envelope-o"></i>'+bootData.user.email+'</a>'+
+    '<p><i class="fa fa-linux"></i>'+bootData.user.cohort_name+'</p>'+
+    '<a href='+bootData.user.linked_in+'target="_blank"><i class="fa fa-linkedin-square"></i>&nbsp</a>'+
+    '<a href='+bootData.user.github+'target="_blank"><i class="fa fa-github-square"></i>&nbsp</a>'+
+    '<a href='+bootData.user.facebook+'target="_blank"><i class="fa fa-facebook-square"></i>&nbsp</a>'+
+    '<a href='+bootData.user.twitter+'target="_blank"><i class="fa fa-twitter-square"></i>&nbsp</a>'+
+    '<a href='+bootData.user.blog+'target="_blank"><i class="fa fa-tumblr-square"></i>&nbsp</a>'+
+  '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  infowindow.open(map,marker);
 }
 
 function handleNoGeolocation(errorFlag) {
@@ -70,7 +90,6 @@ function handleNoGeolocation(errorFlag) {
     position: new google.maps.LatLng(60, 105),
     content: content
   };
-
   var infowindow = new google.maps.InfoWindow(options);
   map.setCenter(options.position);
 }
