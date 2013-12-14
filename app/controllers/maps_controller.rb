ca0838f84724e3e1736574b6accd541eb70aee2f
Steveo1485 <stevennugent@gmail.com>
2013-12-14 01:15:53 -0800

    merged in authentication branch

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,15 @@
 class MapsController < ApplicationController
+  respond_to :json, :html
 
   def index
+    @users = User.all
+    @all_locations = []
+    @users.each {|user| @all_locations << user.location }
+    puts @all_locations
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @all_locations}
+    end
   end
 
 end
