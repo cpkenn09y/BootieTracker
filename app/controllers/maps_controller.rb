45100dee67d2d834e08ff5cbd7fb46cb94a848e5
Elaine Yu <elaine.lee.yu@gmail.com>
2013-12-14 13:17:56 -0800

    refactored maps controller, cleaned up gemfile

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -7,34 +7,35 @@ class MapsController < ApplicationController
       @user= User.find(session[:user_id])
     end
     users = User.all
-    p "????"
-    p users.count
     @user_data = []
-    users.each do |user|
-      gravatar_url = Gravatar.new(user.email).image_url
-      p gravatar_url
-      @user_data << { :user => {
-        name: user.name,
-        image_url: gravatar_url,
-        email: user.email,
-        cohort_name: user.cohort,
-        linked_in: user.linkedin_url,
-        facebook: user.facebook_url,
-        twitter: user.twitter_url,
-        github: user.github_url,
-        blog: user.blog,
-        role: user.role,
-        headline: user.headline,
-        current_location: user.current_location,
-        latitude: user.latitude,
-        longitude: user.longitude,
-        }}
-    end
+    create_user_data_json(users)
 
     respond_to do |format|
       format.html
       format.json { render :json => @user_data}
+      end
     end
+  end
+
+  def create_user_data_json(users)
+    users.each do |user|
+      gravatar_url = Gravatar.new(user.email).image_url
+      @user_data << { :user => {
+      name: user.name,
+      image_url: gravatar_url,
+      email: user.email,
+      cohort_name: user.cohort,
+      linked_in: user.linkedin_url,
+      facebook: user.facebook_url,
+      twitter: user.twitter_url,
+      github: user.github_url,
+      blog: user.blog,
+      role: user.role,
+      headline: user.headline,
+      current_location: user.current_location,
+      latitude: user.latitude,
+      longitude: user.longitude,
+    }}
   end
 
 end
