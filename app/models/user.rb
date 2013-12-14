281fcd80aec1c4a8a049b48cbc9b21f0aae42200
Steveo1485 <stevennugent@gmail.com>
2013-12-14 01:15:53 -0800

    merged in authentication branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+<<<<<<< Updated upstream
   attr_accessible :headline, :location_name, :current_location, :bio, :blog, :cohort_id, :email, :facebook_url, :github_url, :hacker_news_url, :hometown, :linked_in_url, :name, :quora, :twitter_url
 
   before_save :update_location
@@ -9,3 +10,8 @@ class User < ActiveRecord::Base
   end
 
 end
+=======
+  attr_accessible :first_name, :last_name, :location
+
+end
+>>>>>>> Stashed changes
