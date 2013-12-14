4a19e3f6a9c27a5fdd8a58cafeff7b2061cc0630
stephenitis <nguyenhstephen@gmail.com>
2013-12-13 21:03:21 -0800

    initial app

---
+++ README.md
@@ -0,0 +1,13 @@
+# Booty-Tracker
+
+We are creating a mapping of DBC alumni to enable DBC students and alumni to easily see who is within their vicinity
+
+## Team Members
+
+### Kenneth Yu
+
+### Stephen Nguyen
+
+### Elaine Yu
+
+### Stephen Nugent
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootyTracker::Application.config.secret_token = '720f28468829fc10b7a4c8bfdfa077d4adf8b8fc05563a595feb900ca20e58b41a785bcc841497686e8ae7b5ffefa6ee0bd014b1dd255c7ab5c664b6e91e3bee'
