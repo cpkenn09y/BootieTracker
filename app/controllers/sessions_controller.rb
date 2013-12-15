1cfc376aada5e60416b2739d9d33aa44daaab989
Elaine Yu <elaine.lee.yu@gmail.com>
2013-12-14 17:15:33 -0800

    omniauth fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,7 @@ class SessionsController < ApplicationController
     auth = Authentication.find_or_create_from_auth_hash(auth_hash)
     @user =  User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
+    redirect_to root_url
   end
 
 protected
