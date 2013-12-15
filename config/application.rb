9bf7cf89b19981ed6e462cab111da6f91a129519
Kenneth Yu <kenyu@ucdavis.edu>
2013-12-14 19:06:44 -0800

    DotEnv within RescueError

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,10 @@ end
 
 module BootyTracker
   class Application < Rails::Application
-
-    Dotenv.load
-
+    begin
+      Dotenv.load
+      rescue LoadError
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
