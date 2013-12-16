21752655a17ef5342c601d56d9a588069fdf9eb3
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-12-15 19:53:26 -0800

    comment out code

--- app/assets/javascripts/legend.js
+++ app/assets/javascripts/legend.js
@@ -1,8 +1,8 @@
-var legend = document.getElementById('legend');
-for (var style in styles) {
-  var name = style.name;
-  var icon = style.icon;
-  var div = document.createElement('div');
-  div.innerHTML = '<img src="' + icon + '"> ' + name;
-  legend.appendChild(div);
-}
+// var legend = document.getElementById('legend');
+// for (var style in styles) {
+//   var name = style.name;
+//   var icon = style.icon;
+//   var div = document.createElement('div');
+//   div.innerHTML = '<img src="' + icon + '"> ' + name;
+//   legend.appendChild(div);
+// }
