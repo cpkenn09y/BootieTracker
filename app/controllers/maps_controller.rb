278ae275a9f3492b23f432a18995171bbe99ec2f
Kenneth Yu <kenyu@ucdavis.edu>
2014-02-17 15:25:13 -0800

    only run create_user_data_json method if users has data

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -17,26 +17,27 @@ class MapsController < ApplicationController
 
 private
   def create_user_data_json(users)
-    users.each do |user|
-      gravatar_url = Gravatar.new(user.email).image_url
-      @user_data << { :user => {
-      name: user.name,
-      image_url: gravatar_url,
-      email: user.email,
-      cohort_name: user.cohort,
-      linked_in: user.linkedin_url,
-      facebook: user.facebook_url,
-      twitter: user.twitter_url,
-      github: user.github_url,
-      blog: user.blog,
-      role: user.role,
-      headline: user.headline,
-      current_location: user.current_location,
-      git_location: user.git_location,
-      linkedin_location: user.linkedin_location,
-      latitude: user.latitude,
-      longitude: user.longitude,
-    }}
+    if users
+      users.each do |user|
+        gravatar_url = Gravatar.new(user.email).image_url
+        @user_data << { :user => {
+        name: user.name,
+        image_url: gravatar_url,
+        email: user.email,
+        cohort_name: user.cohort,
+        linked_in: user.linkedin_url,
+        facebook: user.facebook_url,
+        twitter: user.twitter_url,
+        github: user.github_url,
+        blog: user.blog,
+        role: user.role,
+        headline: user.headline,
+        current_location: user.current_location,
+        git_location: user.git_location,
+        linkedin_location: user.linkedin_location,
+        latitude: user.latitude,
+        longitude: user.longitude,
+      }}
+    end
   end
-
 end
